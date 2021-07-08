235916435c0750c99da6a696715e1cf92b2b8b24
Matt Briggs <brigmatt@amazon.com>
2021-07-08 12:20:29 -0700

    model: test and resource provider
    Initial implementation of TestSys Test and ResourceProvider using
    kube-rs.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -3,3 +3,5 @@
 This library provides the Kubernetes custom resource definitions and their API clients.
 
 !*/
+
+pub mod model;
---
+++ client/src/model/mod.rs
@@ -0,0 +1,11 @@
+mod resource_provider;
+mod test;
+
+pub use resource_provider::{ResourceProvider, ResourceProviderSpec, ResourceProviderStatus};
+pub use test::{
+    AgentStatus, ControllerStatus, ResourceStatus, RunState, Test, TestResults, TestSpec,
+    TestStatus,
+};
+
+pub const TESTSYS_NAMESPACE: &str = "testsys.bottlerocket.aws";
+pub const TESTSYS_API: &str = "testsys.bottlerocket.aws/v1";
