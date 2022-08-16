e6e57dfaa558ccee64c6f4469371a6c0472b5adb
mjsterckx <msterckx@amazon.com>
2022-08-16 14:43:20 +0000

    Changed printed logs format to be easier to read for humans instead of a byte-string

--- cli/src/logs.rs
+++ cli/src/logs.rs
@@ -37,19 +37,19 @@ impl Logs {
             (Some(test), None, None, false ) => {
                 let mut logs = client.test_logs(test, self.follow).await.context("Unable to get logs.")?;
                 while let Some(line) = logs.next().await {
-                    println!("{:#?}", line.context("Unable to read line")?);
+                    print!("{}", String::from_utf8_lossy(&line.context("Unable to read line")?));
                 }
             }
             (None, Some(resource), Some(state), false) => {
                 let mut logs = client.resource_logs(resource, state, self.follow).await.context("Unable to get logs.")?;
                 while let Some(line) = logs.next().await {
-                    println!("{:#?}", line.context("Unable to read line")?);
+                    print!("{}", String::from_utf8_lossy(&line.context("Unable to read line")?));
                 }
             }
             (None, None, None, true) => {
                 let mut logs = client.controller_logs(self.follow).await.context("Unable to get logs.")?;
                 while let Some(line) = logs.next().await {
-                    println!("{:#?}", line.context("Unable to read line")?);
+                    print!("{}", String::from_utf8_lossy(&line.context("Unable to read line")?));
                 }
             }
             _ => return Err(Error::msg("Invalid arguments were provided. Exactly one of `--test`, `--resource`, and `--controller` must be used.")),
